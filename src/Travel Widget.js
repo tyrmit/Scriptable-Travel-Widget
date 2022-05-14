e72ccb789bcc5e3fcebb2135c224241c08055260
tyrmi <tyrmit@hotmail.com>
2022-05-14 19:12:58 +1000

    Added logging to the TravelWidget.js script

--- src/Travel Widget.js
+++ src/Travel Widget.js
@@ -9,15 +9,28 @@
  */
 'use strict';
 
+const DEBUG = false;
+const logger = DEBUG ? importModule('/lib/Logger').logger : null;
+if (DEBUG) {
+    await logger.openLogFile('DestinationTravelTime.log', false);
+    logger.writeToLogFile('Starting TravelWidget script');
+}
+let logInfo = '';
+
 let routeModule = null;
 try {
     routeModule = importModule('/lib/DestinationTravelTime');
 } catch (err) {
+    if (DEBUG) logger.writeToLogFile(err, null, 'ERROR');
     console.error(err);
 }
 
 if (!routeModule) {
     console.log('No routeModule, exiting');
+    if (DEBUG) {
+        logger.writeToLogFile('No routeModule, exiting');
+        logger.closeLogFile();
+    }
     return;
 }
 
@@ -31,40 +44,44 @@ const routeInfoPromise = routeModule.getTravelTime(true);
  * @param {number} triggerTimeSeconds Date/time in seconds since the UNIX epoch for the notification to trigger
  */
 async function addUpdateNotification(title, message, triggerTimeSeconds) {
+    if (DEBUG) logger.pushFunction('addUpdateNotification');
+
     const triggerTime = new Date();
     triggerTime.setTime(triggerTimeSeconds);
 
-    console.log(
-        `addUpdateNotification('${title}'): function called with:\n\ttitle = '${title}'\n\tmessage = '${message}'\n\ttriggerTime = ${triggerTime}`
-    );
+    logInfo = `addUpdateNotification('${title}'): function called with:\n\ttitle = '${title}'\n\tmessage = '${message}'\n\ttriggerTime = ${triggerTime}`;
+    console.log(logInfo);
+    if (DEBUG) logger.writeToLogFile(logInfo);
 
     // Notification trigger time needs to be more than 30 seconds in the future before we do anything
     if (triggerTime > Date.now() + 30 * 1000) {
-        console.log(
-            `addUpdateNotification('${title}'): triggerTime ${triggerTime} is greater than ${
-                Date.now() + 30 * 1000
-            }, thus proceeding`
-        );
+        logInfo = `addUpdateNotification('${title}'): triggerTime ${triggerTime} is greater than ${
+            Date.now() + 30 * 1000
+        }, thus proceeding`;
+        console.log(logInfo);
+        if (DEBUG) logger.writeToLogFile(logInfo);
 
         const notifications = await Notification.allPending();
         const currentNotification = notifications.find(
             (notif) => notif.title === title
         );
         if (currentNotification) {
-            console.log(
-                `addUpdateNotification('${title}'): notification with title '${title}' already exists:\n\tmessage = '${currentNotification.body}'\n\ttriggerTime = ${currentNotification.nextTriggerDate}`
-            );
-            console.log(
-                `addUpdateNotification('${title}'): updating existing notification '${title}' with:\n\tmessage = '${message}'\n\ttriggerTime = ${triggerTime}`
-            );
+            logInfo = `addUpdateNotification('${title}'): notification with title '${title}' already exists:\n\tmessage = '${currentNotification.body}'\n\ttriggerTime = ${currentNotification.nextTriggerDate}`;
+            console.log(logInfo);
+            if (DEBUG) logger.writeToLogFile(logInfo);
+
+            logInfo = `addUpdateNotification('${title}'): updating existing notification '${title}' with:\n\tmessage = '${message}'\n\ttriggerTime = ${triggerTime}`;
+            console.log(logInfo);
+            if (DEBUG) logger.writeToLogFile(logInfo);
 
             currentNotification.setTriggerDate(triggerTime);
             currentNotification.body = message;
             currentNotification.schedule();
         } else {
-            console.log(
-                `addUpdateNotification('${title}'): no current notification, so need to set one:\n\ttitle = '${title}'\n\tmessage = '${message}'\n\ttriggerTime = ${triggerTime}`
-            );
+            logInfo = `addUpdateNotification('${title}'): no current notification, so need to set one:\n\ttitle = '${title}'\n\tmessage = '${message}'\n\ttriggerTime = ${triggerTime}`;
+            console.log(logInfo);
+            if (DEBUG) logger.writeToLogFile(logInfo);
+
             const notif = new Notification();
             notif.title = title;
             notif.body = message;
@@ -74,12 +91,14 @@ async function addUpdateNotification(title, message, triggerTimeSeconds) {
         }
         return currentNotification;
     } else {
-        console.log(
-            `addUpdateNotification('${title}'): triggerTime ${triggerTime} is NOT greater than ${
-                Date.now() + 30 * 1000
-            }, thus doing nothing`
-        );
+        logInfo = `addUpdateNotification('${title}'): triggerTime ${triggerTime} is NOT greater than ${
+            Date.now() + 30 * 1000
+        }, thus doing nothing`;
+        console.log(logInfo);
+        if (DEBUG) logger.writeToLogFile(logInfo);
     }
+
+    if (DEBUG) logger.popFunction();
 }
 
 // Start setting up the widget here before we need the routeInfo
@@ -105,7 +124,9 @@ const subStack = widget.addStack();
 
 // Now wait for the routeInfo
 const routeInfo = await routeInfoPromise;
-console.log(JSON.stringify(routeInfo));
+logInfo = JSON.stringify(routeInfo);
+console.log(logInfo);
+if (DEBUG) logger.writeToLogFile(logInfo);
 
 // Calculate the notification trigger time amd set the notification. If the trigger time is more than 10 minutes away, also set a notification 10 mins before
 if (routeInfo.arrivalTargetTime) {
@@ -140,12 +161,17 @@ if (routeInfo.arrivalTargetTime) {
 // Finish the widget with the routeInfo
 const routeTimeMinutes = Math.ceil(routeInfo.routeTimeSeconds / 60);
 
+if (DEBUG)
+    logger.writeToLogFile(
+        `Adding text to headerStack: ${routeInfo.destinationName}`
+    );
 const headerText = headerStack.addText(routeInfo.destinationName);
 headerText.font = Font.mediumSystemFont(16);
 
-const mainText = mainNumberStack.addText(
-    routeTimeMinutes.toString().padStart(2, '0')
-);
+const minutesText = routeTimeMinutes.toString().padStart(2, '0');
+if (DEBUG)
+    logger.writeToLogFile(`Adding text to mainNumberStack: ${minutesText}`);
+const mainText = mainNumberStack.addText(minutesText);
 mainText.font = Font.mediumSystemFont(48);
 
 if (routeInfo.arrivalTime && routeInfo.arrivalTargetTime) {
@@ -158,9 +184,12 @@ if (routeInfo.arrivalTime && routeInfo.arrivalTargetTime) {
     mainText.textColor = textColor;
 }
 
+if (DEBUG) logger.writeToLogFile("Adding text to mainMinStack: ' mins'");
 const minText = mainMinStack.addText(' mins');
 minText.font = Font.mediumSystemFont(24);
 
+if (DEBUG)
+    logger.writeToLogFile(`Adding text to subStack: '${routeInfo.routeName}'`);
 const subText = subStack.addText(`Using ${routeInfo.routeName}`);
 subText.font = Font.mediumSystemFont(12);
 
@@ -171,12 +200,26 @@ if (routeInfo.arrivalTime && routeInfo.arrivalTargetTime) {
         routeInfo.arrivalTargetTime.getTime() -
             routeInfo.routeTimeSeconds * 2 * 1000
     );
+    if (DEBUG)
+        logger.writeToLogFile(`Set refreshTime variable to [${refreshTime}]`);
+} else {
+    if (DEBUG)
+        logger.writeToLogFile(
+            'No arrivalTime or arrivalTargetTime set in routeInfo',
+            null,
+            'ERROR'
+        );
 }
+
 if (refreshTime.getTime() < Date.now() + 5 * 60 * 1000) {
     refreshTime.setTime(Date.now());
     refreshTime.setMinutes(refreshTime.getMinutes() + 5);
 }
-console.log('setting widget.refreshAfterDate to ' + refreshTime);
+
+logInfo = 'setting widget.refreshAfterDate to ' + refreshTime;
+console.log(logInfo);
+if (DEBUG) logger.writeToLogFile(logInfo);
+
 widget.refreshAfterDate = refreshTime;
 
 Script.setWidget(widget);
